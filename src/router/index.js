11204d8303ac411751541588fae1b612d67d510c
serapple <serinamgil@gmail.com>
2023-09-09 17:17:11 +0900

    router 설정

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 // Composables
 import { createRouter, createWebHistory } from "vue-router";
+import { useAuthStore } from "@/store";
 
 const routes = [
   {
@@ -20,33 +21,38 @@ const routes = [
         path: "/dashboard",
         name: "dashboard",
         component: () => import("@/views/DashboardView.vue"),
+        meta: { requiresAuth: true, role: "ROLE_ADMIN" },
       },
       {
         path: "/usermgmt",
         name: "usermgmt",
         component: () => import("@/views/UserManagementView.vue"),
+        meta: { requiresAuth: true, role: "ROLE_ADMIN" },
       },
       {
         path: "/usermgmt/:userId",
         name: "userdetails",
         component: () => import("@/views/UserDetailsView.vue"),
+        meta: { requiresAuth: true, role: "ROLE_ADMIN" },
       },
       {
-        
         path: "/mypage",
         name: "mypage",
         component: () => import("@/views/MyPageView.vue"),
+        meta: { requiresAuth: true, role: "ROLE_USER" },
         props: true,
       },
       {
         path: "/reserved/:ticketId",
         name: "reserved",
         component: () => import("@/views/ReservedView.vue"),
+        meta: { requiresAuth: true, role: "ROLE_USER" },
       },
       {
         path: "/seat/:concertId",
         name: "seat",
         component: () => import("@/views/SeatView.vue"),
+        meta: { requiresAuth: true, role: "ROLE_USER" },
       },
     ],
   },
@@ -71,6 +77,25 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach(async (to, from, next) => {
+  const authStore = useAuthStore(); // useAuthStore를 사용
+  if (to.name === "main" && authStore.role === "ROLE_ADMIN") {
+    return next({ path: "/dashboard" });
+  }
+
+  if (to.meta.requiresAuth && !authStore.isAuthenticated) {
+    // 로그인이 필요한 페이지인지 확인
+    return next({ path: "/login" }); // 로그인 페이지로 리다이렉트
+  }
+
+  if (to.meta.role && authStore.role !== to.meta.role) {
+    // 특정 역할이 필요한 경우
+    return next({ path: "/" }); // 권한이 없으면 unauthorized 페이지로 이동
+  }
+
+  next();
 });
 
 export default router;
--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -11,7 +11,13 @@ export const useAuthStore = defineStore("auth", {
       resultCode: "",
     },
     token: window.sessionStorage.getItem("token") || "",
+    role: window.sessionStorage.getItem("role") || "",
   }),
+  getters: {
+    isAuthenticated() {
+      return this.token !== ""; // token이 존재하면 로그인된 것으로 판단
+    },
+  },
   actions: {
     signup(email, userPwd, name, birth, fanCd) {
       api
@@ -28,7 +34,9 @@ export const useAuthStore = defineStore("auth", {
         .login(email, userPwd)
         .then((res) => {
           this.token = res.data.data.accessToken;
+          this.role = res.data.data.role;
           window.sessionStorage.setItem("token", this.token);
+          window.sessionStorage.setItem("role", this.role);
         })
         .catch((err) => {
           console.log(err);
