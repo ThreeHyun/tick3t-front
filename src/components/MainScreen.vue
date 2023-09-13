574026324f51bfab52d552cbab49b30809bb7007
jhyu <uzuhyeon@gmail.com>
2023-09-13 14:34:18 +0900

    [fix] main vue font, margin

--- src/components/MainScreen.vue
+++ src/components/MainScreen.vue
@@ -3,19 +3,19 @@
     <v-container>
       <v-slide-group class="pa-15" center-active show-arrows>
         <v-slide-item v-for="concert in concertList" :key="concert.concertId" class="ma-8">
-          <v-card elevation="5" width="400">
+          <v-card elevation="5" width="400" style="padding: 20px;">
             <v-img :src="concert.imgUrl" height="400px"></v-img>
             <v-card-text>
-              <h3 class="text-h6 mb-3">{{ concert.title }}</h3>
+              <h3 class="text3 mb-3">{{ concert.title }}</h3>
               <hr />
-              <div class="font-weight-light text-h8 mt-2 mb-5">
+              <div class="font-weight-light text3 mt-2 mb-5">
                 <span style="float: left">{{ concert.date }}</span>
                 <span style="float: right">{{ concert.hallName }}</span>
               </div>
             </v-card-text>
             <v-card-actions>
               <v-spacer />
-              <v-btn class="reserve" block rounded="lg" center size="x-large"
+              <v-btn class="reserve2" block rounded="lg" center size="x-large"
                 @click="goToConcert(concert.concertId)">상세보기</v-btn>
             </v-card-actions>
           </v-card>
